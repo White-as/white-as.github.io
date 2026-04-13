e2c71664248ee01febf3ca1d3e88a9299d3b9038
White-as <duok10pesu@protonmail.com>
2026-04-13 18:02:51 +0800

    Update data.js

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const cars = [
     name: "Audi 100",
     owner: "Diani",
     price: 500,
-    hp: 510,
+    kw: 510,
     odo: 0,
     img: "https://talpix.lt/uploads/69db9be0b7b53.jpeg"
   },
@@ -12,7 +12,7 @@ const cars = [
     name: "Mercedes Pimp",
     owner: "Piet",
     price: 90000,
-    hp: 66,
+    kw: 66,
     odo: 0,
     img: "http://talpix.lt/uploads/69db9f64441aa.jpeg"
   },
@@ -20,7 +20,7 @@ const cars = [
     name: "Audi Q5",
     owner: "Andrius",
     price: 15000,
-    hp: 450,
+    kw: 450,
     odo: 0,
     img: "http://talpix.lt/uploads/69dba261045fe.jpg"
   },
@@ -28,7 +28,7 @@ const cars = [
     name: "Asfalt machine",
     owner: "Niko",
     price: 30000,
-    hp: 2045,
+    kw: 2045,
     odo: 0,
     img: "http://talpix.lt/uploads/69dba2dde8103.jpeg"
   },
@@ -36,7 +36,7 @@ const cars = [
     name: "Mercedes C63",
     owner: "Arian",
     price: 100,
-    hp: 500,
+    kw: 500,
     odo: 0,
     img: "http://talpix.lt/uploads/69dba07134179.jpeg"
   }

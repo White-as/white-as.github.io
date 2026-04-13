59314b80a23ee5fd58336ea2b4a3e8bd54b6cb59
White-as <duok10pesu@protonmail.com>
2026-04-13 17:51:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-test
+let currentCars = [...cars];
+
+function render() {
+  const list = document.getElementById("list");
+  list.innerHTML = "";
+
+  currentCars.forEach(car => {
+    const div = document.createElement("div");
+    div.className = "card";
+
+    div.innerHTML = `
+      <img src="${car.img}" />
+      <div>${car.name}</div>
+      <div>Owner: ${car.owner}</div>
+      <div>Price: €${car.price}</div>
+      <div>HP: ${car.hp}</div>
+    `;
+
+    list.appendChild(div);
+  });
+}
+
+function sortByPrice() {
+  currentCars.sort((a, b) => a.price - b.price);
+  render();
+}
+
+function sortByHP() {
+  currentCars.sort((a, b) => b.hp - a.hp);
+  render();
+}
+
+function resetData() {
+  currentCars = [...cars];
+  render();
+}
+
+render();

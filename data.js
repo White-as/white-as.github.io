01f4047707f6a7ba2fec0884adbfd219957a0e4a
White-as <duok10pesu@protonmail.com>
2026-04-13 22:40:33 +0800

    Update data.js

--- data.js
+++ data.js
@@ -4,48 +4,48 @@ const cars = [
     name: "Audi 100",
     owner: "Diani",
     price: 500,
-    kw: 510,
-    odo: 0,
+    power: 510,
+    odometer: 0,
     img: "https://talpix.lt/uploads/69db9be0b7b53.jpeg"
   },
   {
     name: "Mercedes Pimp",
     owner: "Piet",
     price: 90000,
-    kw: 66,
-    odo: 0,
+    power: 66,
+    odometer: 0,
     img: "http://talpix.lt/uploads/69db9f64441aa.jpeg"
   },
   {
     name: "Audi Q5",
     owner: "Andrius",
     price: 15000,
-    kw: 450,
-    odo: 0,
+    power: 450,
+    odometer: 0,
     img: "http://talpix.lt/uploads/69dba261045fe.jpg"
   },
   {
     name: "Asfalt machine",
     owner: "Niko",
     price: 30000,
-    kw: 2045,
-    odo: 0,
+    power: 2045,
+    odometer: 0,
     img: "http://talpix.lt/uploads/69dba2dde8103.jpeg"
   },
   {
     name: "Mercedes C63",
     owner: "Arian",
     price: 100,
-    kw: 500,
-    odo: 0,
+    power: 500,
+    odometer: 0,
     img: "http://talpix.lt/uploads/69dba07134179.jpeg"
   },
   {
-   name: "Honda",
+   name: "BMW M4",
     owner: "Alexi",
     price: 666,
-    kw: 2,
-    odo: 0,
+    power: 2,
+    odometer: 0,
     img: "http://talpix.lt/uploads/69dcedc21e3d4.jpeg"
   }
 ];

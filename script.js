fd942d3c30cc81500d76f5db207d23d8bd4d1716
White-as <duok10pesu@protonmail.com>
2026-04-15 13:17:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function render() {
     div.className = "card";
 
     div.innerHTML = `
-    <div class= "info">Score: ${(calculateScore(car, currentCars)*100).toFixed(1)} </div>
+    <div class="info">Score: ${(calculateScore(car, currentCars)*100).toFixed(1)} </div>
       <div class="card-top">
         <img src="${car.img}" class="car-image" />
 
@@ -74,7 +74,7 @@ function calculateScore(car, cars) {
   return total;
 }
 
-  function sortByscore() {
+  function sortByScore() {
     currentCars.sort((a, b) => 
                     calculateScore (b, currentCars) - calculateScore(a, currentCars)); 
     render();
